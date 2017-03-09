0e0826bf9c2305043272b0d2eed31a33370c379c
lion137 <lion137@gmail.com>
2017-03-09 00:50:21 +0000

    Update tree_functions.scala

--- tree_functions.scala
+++ tree_functions.scala
@@ -1,16 +1,16 @@
-ef treeMap[A, B](tree: Tree[A])(f: A => B): Tree[B] =
+def treeMap[A, B](tree: Tree[A])(f: A => B): Tree[B] =
 		tree match {
 			case EmptyTree => EmptyTree
 			case Node(x, left, right) => Node(f(x), tree2Map(left)(f), tree2Map(right)(f))
 		}                               
 	
-	def length[A](tree: Tree[A]): Int =
+def length[A](tree: Tree[A]): Int =
 	 tree match {
 	 	case EmptyTree => 0
 	 	case Node(x, left, right) => length2(left) + 1 + length2(right)
 	 }                                    
 	 
-	 def maxDepth[A](tree: Tree[A]): Int =
+def maxDepth[A](tree: Tree[A]): Int =
 	   tree match {
 	   	case EmptyTree => 0
 	   	case Node(x, left, right) =>
@@ -20,32 +20,31 @@ ef treeMap[A, B](tree: Tree[A])(f: A => B): Tree[B] =
 	   		 else rmax + 1
 	   }                                      
 	  
-	  def loadTree[A](tree: Tree[A]): A =
+def loadTree[A](tree: Tree[A]): A =
 	  	tree match {
 	  		case EmptyTree => throw new Exception("value of empty tree")
 	  		case Node(x, left, right) => x
 	  	}                            
 	  
-	  def leftBranch[T](tree: Tree[T]): Tree[T] =
+def leftBranch[T](tree: Tree[T]): Tree[T] =
 	   tree match {
 	   	case EmptyTree => throw new Exception("left branch of empty tree")
 	   	case Node(x, left, right) => left
 	   	}                                
 	   
-	   def rightBranch[T](tree: Tree[T]): Tree[T] =
+def rightBranch[T](tree: Tree[T]): Tree[T] =
 	   tree match {
 	   	case EmptyTree => throw new Exception("left branch of empty tree")
 	   	case Node(x, left, right) => right
 	   	}                              
 	  
-	  def isTreeEmpty[T](tree: Tree[T]): Boolean =
+def isTreeEmpty[T](tree: Tree[T]): Boolean =
 	  	tree match {
 	  		case EmptyTree => true
 	  		case Node(x, left, right) => false
 	  	}                               
 	 
-	 
-	 def addToTree(elem: Int, tree: Tree[Int]): Tree[Int] = {
+def addToTree(elem: Int, tree: Tree[Int]): Tree[Int] = {
 	 	
 	 		if (isTreeEmpty(tree)) Node(elem, EmptyTree, EmptyTree)
 	 		else if (elem == loadTree(tree)) tree
